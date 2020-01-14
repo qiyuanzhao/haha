9afcb65b8b97e6fb8ac6fc3783dae44616c96e0e
qiyuanzhao <yuanzhao.qi@lavector.com>
2020-01-14 16:40:02 +0800

    add a newClass of new branch

---
+++ src/main/java/com/constant/GitTest.java
@@ -0,0 +1,19 @@
+package com.constant;
+
+/**
+ * Created by qyz on 2020/1/14.
+ */
+public class GitTest {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("这是新分支");
+
+
+    }
+
+
+
+}
